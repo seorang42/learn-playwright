c24a9e849c03a69f9cb251b9605e954f54ef5e21
seorang42 <seorang42@naver.com>
2026-02-14 18:29:41 +0900

    웹 친화적인 작동방식

--- __tests__/e2e/navigation.spec.ts
+++ __tests__/e2e/navigation.spec.ts
@@ -10,6 +10,13 @@ test("if user visit home and click 'Get Started', h1 'introduction' is visible a
   await page.goto(START_URL);
   await page.getByRole("link", { name: "Get Started" }).click();
 
+  // 화면에 렌더링 되기 전 isVisible을 결정해 테스트 실패 (불안정한 테스트)
+  // const isVisible = await page
+  //   .getByRole("heading", { name: H1, level: 1 })
+  //   .isVisible();
+  // expect(isVisible).toEqual(true);
+  // => Web-first Assertions를 사용 권장
+
   await expect(page.getByRole("heading", { name: H1, level: 1 })).toBeVisible();
   await expect(page).toHaveTitle(TITLE);
 });
